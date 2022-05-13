928d78a60556610f88490ff8d9c50eb2636a644f
JoyleneKirui <joylene.kirui@student.moringaschool.com>
2022-05-13 11:19:38 +0300

    add template files

---
+++ app/routes.py
@@ -0,0 +1,68 @@
+from flask import render_template, url_for, flash, redirect, request
+from app import app, db, bcrypt
+from app.forms import LoginForm, RegistrationForm, LoginForm
+from app.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
+ 
+
+posts = [
+    {
+        'author': 'Joy Kirui',
+        'title': 'My Hobbies',
+        'content': 'Coffee and Code',
+        'date': '12:08:2020'
+    },
+    {
+        'author': 'Faith Kirui',
+        'title': 'What I lovee',
+        'content': 'Coffee and Code',
+        'date': '10:08:2020'  
+    }
+]
+@app.route("/home")
+@app.route("/")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title = 'About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='Account')
