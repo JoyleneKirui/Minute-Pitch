abdb1ca3127b445b900796e300a3eb900da99e2d
JoyleneKirui <joylene.kirui@student.moringaschool.com>
2022-05-14 14:49:41 +0300

    edit class Post

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from app import db, login_manager
 from datetime import datetime
 from flask_login import UserMixin
@@ -12,10 +13,12 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
+    bio = db.Column(db.Text, nullable=False, default='Hey, lets pitch')
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
 
 
 class Post(db.Model):
