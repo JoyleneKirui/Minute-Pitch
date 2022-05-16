54916123a975c09916e6b6fa1662199cb5e4c828
JoyleneKirui <joylene.kirui@student.moringaschool.com>
2022-05-16 07:48:10 +0300

    tweak app for heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask 
-from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager 
@@ -8,13 +7,12 @@ from flask_fontawesome import FontAwesome
 
 app = Flask(__name__)
 fa = FontAwesome(app)
-mail = Mail()
-mail.init_app(app)
+db = SQLAlchemy(app)
 
 app.config['SECRET_KEY'] = '12345678'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
-db = SQLAlchemy(app)
+
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
--- manage.py
+++ manage.py
@@ -1,5 +1,8 @@
-from app import app
-
+from app import app, db
+from flask_migrate import Migrate, MigrateCommand
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+migrate = Migrate(app, db)
+manager.add_command('db',MigrateCommand)
